6009aa7a25b2e861c41f6637ab80d84ec58f47ca
GlenAlan <glen.pearce@outlook.com>
2024-08-02 15:44:24 +1000

    init

---
+++ Automatic_Monolayer_Finder.py
@@ -0,0 +1,228 @@
+"""
+Camera Live View - TkInter
+
+This example shows how one could create a live image viewer using TkInter.
+It also uses the third party library 'pillow', which is a fork of PIL.
+
+This example detects if a camera is a color camera and will process the
+images using the tl_mono_to_color_processor module.
+
+This example uses threading to enqueue images coming off the camera in one thread, and
+dequeue them in the UI thread for quick displaying.
+
+"""
+
+try:
+    # if on Windows, use the provided setup script to add the DLLs folder to the PATH
+    from windows_setup import configure_path
+    configure_path()
+except ImportError:
+    configure_path = None
+
+from thorlabs_tsi_sdk.tl_camera import TLCameraSDK, TLCamera, Frame
+from thorlabs_tsi_sdk.tl_camera_enums import SENSOR_TYPE
+from thorlabs_tsi_sdk.tl_mono_to_color_processor import MonoToColorProcessorSDK
+
+from MCM301_COMMAND_LIB import *
+import time
+import random
+
+from custom_definitions import *
+
+import tkinter as tk
+from PIL import Image, ImageTk
+import typing
+import threading
+import queue
+    
+    
+
+
+
+class LiveViewCanvas(tk.Canvas):
+
+    def __init__(self, parent, image_queue):
+        # type: (typing.Any, queue.Queue) -> LiveViewCanvas
+        self.image_queue = image_queue
+        self._image_width = 0
+        self._image_height = 0
+
+        tk.Canvas.__init__(self, parent)
+        self.grid(column=0, row=0, rowspan=4, columnspan=2, sticky=tk.E)
+        self._get_image()
+
+    def _get_image(self):
+        try:
+            image = self.image_queue.get_nowait()
+            
+            # resize image
+            image = image.resize((500,500))
+            
+            self._image = ImageTk.PhotoImage(master=self, image=image)
+            if (self._image.width() != self._image_width) or (self._image.height() != self._image_height):
+                # resize the canvas to match the new image size
+                self._image_width = 500 #self._image.width()
+                self._image_height = 500 #self._image.height()
+                self.config(width=self._image_width, height=self._image_height)
+            self.create_image(0, 0, image=self._image, anchor='nw')
+        except queue.Empty:
+            pass
+        self.after(10, self._get_image)
+
+
+class ImageAcquisitionThread(threading.Thread):
+
+    def __init__(self, camera):
+        # type: (TLCamera) -> ImageAcquisitionThread
+        super(ImageAcquisitionThread, self).__init__()
+        self._camera = camera
+        self._previous_timestamp = 0
+
+        # setup color processing if necessary
+        if self._camera.camera_sensor_type != SENSOR_TYPE.BAYER:
+            # Sensor type is not compatible with the color processing library
+            self._is_color = False
+        else:
+            self._mono_to_color_sdk = MonoToColorProcessorSDK()
+            self._image_width = self._camera.image_width_pixels
+            self._image_height = self._camera.image_height_pixels
+            self._mono_to_color_processor = self._mono_to_color_sdk.create_mono_to_color_processor(
+                SENSOR_TYPE.BAYER,
+                self._camera.color_filter_array_phase,
+                self._camera.get_color_correction_matrix(),
+                self._camera.get_default_white_balance_matrix(),
+                self._camera.bit_depth
+            )
+            self._is_color = True
+
+        self._bit_depth = camera.bit_depth
+        self._camera.image_poll_timeout_ms = 0  # Do not want to block for long periods of time
+        self._image_queue = queue.Queue(maxsize=2)
+        self._stop_event = threading.Event()
+
+    def get_output_queue(self):
+        return self._image_queue
+
+    def stop(self):
+        self._stop_event.set()
+
+    def _get_color_image(self, frame):
+        # type: (Frame) -> Image
+        # verify the image size
+        width = frame.image_buffer.shape[1]
+        height = frame.image_buffer.shape[0]
+        if (width != self._image_width) or (height != self._image_height):
+            self._image_width = width
+            self._image_height = height
+            print("Image dimension change detected, image acquisition thread was updated")
+        # color the image. transform_to_24 will scale to 8 bits per channel
+        color_image_data = self._mono_to_color_processor.transform_to_24(frame.image_buffer,
+                                                                         self._image_width,
+                                                                         self._image_height)
+        color_image_data = color_image_data.reshape(self._image_height, self._image_width, 3)
+        # return PIL Image object
+        return Image.fromarray(color_image_data, mode='RGB')
+
+    def _get_image(self, frame):
+        # type: (Frame) -> Image
+        # no coloring, just scale down image to 8 bpp and place into PIL Image object
+        scaled_image = frame.image_buffer >> (self._bit_depth - 8)
+        return Image.fromarray(scaled_image)
+
+    def run(self):
+        while not self._stop_event.is_set():
+            try:
+                frame = self._camera.get_pending_frame_or_null()
+                if frame is not None:
+                    if self._is_color:
+                        pil_image = self._get_color_image(frame)
+                    else:
+                        pil_image = self._get_image(frame)
+                    self._image_queue.put_nowait(pil_image)
+            except queue.Full:
+                # No point in keeping this image around when the queue is full, let's skip to the next one
+                pass
+            except Exception as error:
+                print("Encountered error: {error}, image acquisition will stop.".format(error=error))
+                break
+        print("Image acquisition has stopped")
+        if self._is_color:
+            self._mono_to_color_processor.dispose()
+            self._mono_to_color_sdk.dispose()
+
+def test_move():
+    mcm301obj = MCM301()
+
+    devs = MCM301.list_devices()
+    print(devs)
+    if len(devs) <= 0:
+        print('There is no devices connected')
+        exit()
+    device_info = devs[0]
+    sn = device_info[0]
+    print("connect ", sn)
+    hdl = mcm301obj.open(sn, 115200, 3)
+    if hdl < 0:
+        print("open ", sn, " failed. hdl is ", hdl)
+        exit()
+    if mcm301obj.is_open(sn) == 0:
+        print("MCM301IsOpen failed")
+        mcm301obj.close()
+        exit()
+
+    step_size = 1000
+    for stage_num in [4, 5, 6]:
+        result = mcm301obj.set_jog_params(stage_num, step_size)
+        if result < 0:
+            print("set_jog_params failed")
+        else:
+            print("set_jog_params:", step_size)
+
+    for i in range(20):
+        stage_num, stage_direction = random.randint(4, 5), random.randint(0, 1)
+        mcm301obj.move_jog(stage_num, stage_direction)
+        print(f"Moivng stage {stage_num}, {'clockwise'*stage_direction + 'counter-clockwise'*(1-stage_direction)}")
+        time.sleep(2)
+
+
+    
+""" Main
+
+When run as a script, a simple Tkinter app is created with just a LiveViewCanvas widget. 
+
+"""
+if __name__ == "__main__":
+
+    with TLCameraSDK() as sdk:
+        camera_list = sdk.discover_available_cameras()
+        with sdk.open_camera(camera_list[0]) as camera:
+
+            # create generic Tk App with just a LiveViewCanvas widget
+            print("Generating app...")
+            root = tk.Tk()
+            root.title(camera.name)
+            image_acquisition_thread = ImageAcquisitionThread(camera)
+            camera_widget = LiveViewCanvas(parent=root, image_queue=image_acquisition_thread.get_output_queue())
+            vel = 50            
+        
+            print("Setting camera parameters...")
+            camera.frames_per_trigger_zero_for_unlimited = 0
+            camera.arm(2)
+            camera.issue_software_trigger()
+
+            print("Starting image acquisition thread...")
+            image_acquisition_thread.start()
+
+            test_thread = threading.Thread(target=test_move)
+            test_thread.start()
+
+            print("App starting")
+            root.mainloop()
+
+            print("Waiting for image acquisition thread to finish...")
+            image_acquisition_thread.stop()
+            image_acquisition_thread.join()
+
+            print("Closing resources...")
+
+    print("App terminated. Goodbye!")
