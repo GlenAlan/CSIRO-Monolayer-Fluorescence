c4acb6b3415504f35b18f135e69dcdf4ebb2a277
GlenAlan <glen.pearce@outlook.com>
2024-08-02 14:51:57 +1000

    Added basic stage movement

--- basic_stage.py
+++ basic_stage.py
@@ -1,5 +1,6 @@
 from MCM301_COMMAND_LIB import *
 import time
+import random
 
 print("*** MCM301 device python example ***")
 mcm301obj = MCM301()
@@ -20,4 +21,17 @@ if mcm301obj.is_open(sn) == 0:
     print("MCM301IsOpen failed")
     mcm301obj.close()
     exit()
-input()
+
+step_size = 5000
+for stage_num in [4, 5, 6]:
+    result = mcm301obj.set_jog_params(stage_num, step_size)
+    if result < 0:
+        print("set_jog_params failed")
+    else:
+        print("set_jog_params:", step_size)
+
+for i in range(10):
+    stage_num, stage_direction = random.randint(4, 6), random.randint(0, 1)
+    mcm301obj.move_jog(stage_num, stage_direction)
+    print(f"Moivng stage {stage_num}, {'clockwise'*stage_direction + 'counter-clockwise'*(1-stage_direction)}")
+    time.sleep(1)
