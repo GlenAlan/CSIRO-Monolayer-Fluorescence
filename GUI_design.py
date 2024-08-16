3ae3420dcd91a97356c5011f548c4e2e9733a382
Lachlan Catto <lavalachlan@gmail.com>
2024-08-16 14:27:25 +1000

    Added move control and position buttons

--- GUI_design.py
+++ GUI_design.py
@@ -1,14 +1,6 @@
 """
-Camera Live View - TkInter
 
-This example shows how one could create a live image viewer using TkInter.
-It also uses the third party library 'pillow', which is a fork of PIL.
-
-This example detects if a camera is a color camera and will process the
-images using the tl_mono_to_color_processor module.
-
-This example uses threading to enqueue images coming off the camera in one thread, and
-dequeue them in the UI thread for quick displaying.
+GUI design using Tkinter with image, button controls for controlling position, and a whole tab for calibration.
 
 """
 
@@ -19,48 +11,99 @@ try:
 except ImportError:
     configure_path = None
 
-# from thorlabs_tsi_sdk.tl_camera import TLCameraSDK, TLCamera, Frame
-# from thorlabs_tsi_sdk.tl_camera_enums import SENSOR_TYPE
-# from thorlabs_tsi_sdk.tl_mono_to_color_processor import MonoToColorProcessorSDK
+from thorlabs_tsi_sdk.tl_camera import TLCameraSDK, TLCamera, Frame
+from thorlabs_tsi_sdk.tl_camera_enums import SENSOR_TYPE
+from thorlabs_tsi_sdk.tl_mono_to_color_processor import MonoToColorProcessorSDK
 
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
-import typing
-import threading
-import queue
+
+from MCM301_COMMAND_LIB import *
+
+
+def move_and_wait(mcm301obj, pos, stage=(4, 5)):
+    """
+    Moves the stage to a specified position and waits for the movement to complete.
+
+    Args:
+        mcm301obj (MCM301): The MCM301 object that controls the stage.
+        pos (tuple): The desired position to move to, given as a tuple of x and y coordinates in nanometers.
+        stage (tuple): The stages to move, represented by integers between 4 and 6 (e.g., 4 for X-axis and 5 for Y-axis).
     
-""" Main
+    The function converts the given nanometer position into encoder units that the stage controller can use,
+    then commands the stage to move to those positions. It continues to check the status of the stage 
+    until it confirms that the movement is complete.
+    """
+    x_nm, y_nm, = pos
+    print(f"Moving to {x_nm}, {y_nm}")
+    x, y = [0], [0]
+    stage_x, stage_y = stage
+    encoder_val_x, bits_x = [0], [0]
+    encoder_val_y, bits_y = [0], [0]
 
-GUI design with image, button controls for controlling position, and a whole tab for calibration.
+    # Convert the positions from nanometers to encoder units
+    mcm301obj.convert_nm_to_encoder(stage_x, x_nm, x)
+    mcm301obj.convert_nm_to_encoder(stage_y, y_nm, y)
 
+    # Move the stages to the required encoder position
+    mcm301obj.move_absolute(stage_x, x[0])
+    mcm301obj.move_absolute(stage_y, y[0])
+
+    # Wait until the stages have finished moving by checking the status bits
+    while bits_x[0] not in confirmation_bits or bits_y[0] not in confirmation_bits:
+        mcm301obj.get_mot_status(stage_x, encoder_val_x, bits_x)
+        mcm301obj.get_mot_status(stage_y, encoder_val_y, bits_y)
+        # print(f"x: {bits_x}, y:{bits_y}")
+
+def get_pos(mcm301obj, stages=[4, 5, 6]):
+    """
+    Retrieves the current position of the specified stages.
+
+    Args:
+        mcm301obj (MCM301): The MCM301 object that controls the stage.
+        stages (list): A list of stage numbers for which the positions are to be retrieved.
+    
+    Returns:
+        pos (list): A list of positions corresponding to the specified stages, 
+                    in nanometers.
+    
+    The function queries the current encoder value for each specified stage, 
+    converts that value into nanometers, and returns the positions as a list.
+    """
+    pos = []
+    for stage in stages:
+        encoder_val, nm = [0], [0]
+
+         # Get the current encoder value for the stage
+        mcm301obj.get_mot_status(stage, encoder_val, [0])
+
+        # Convert the encoder value to nanometers
+        mcm301obj.convert_encoder_to_nm(stage, encoder_val[0], nm)
+
+        # Append the position to the list
+        pos.append(nm[0])
+    return pos
+
+    
+""" 
+Main
 """
 if __name__ == "__main__":
-    # The following is opening the camera which is not used in this case.
-    # with TLCameraSDK() as sdk:
-    #     camera_list = sdk.discover_available_cameras()
-    #     with sdk.open_camera(camera_list[0]) as camera:
-
-    root = tk.Tk() #Create an instance of a window
+    # Create an instance of a window
+    root = tk.Tk()
     root.title('Camera with controls - development')
-
-    image_path = "CSIRO-Monolayer-Fluorescence\Images\Screenshot 2024-07-16 154534.png"
-    #gets the image in a ImageTK type
-    img = ImageTk.PhotoImage(Image.open(image_path).resize((400,400))) #Need to check if resize affects calibration/measurement.
-    stitched_img = ImageTk.PhotoImage(Image.open(image_path).resize((400,400)))
 
     # Create a Notebook (tab container)
     notebook = ttk.Notebook(root)
     notebook.pack(expand=True, fill="both")
 
-    # Creating the tabs
+    # Creating a tab for main. The code is organised by tabs
     tab_main = ttk.Frame(notebook)
     notebook.add(tab_main, text="Main Tab")
-    tab_calibrate = ttk.Frame(notebook)
-    notebook.add(tab_calibrate, text="Calibration Tab")
 
-    # Creating the frames in the tabs
-    frame_image = tk.Frame(master = tab_main)
+    # Creating the frames for the main tab. Note: this does not include anything inside the frames yet.
+    frame_image = tk.Canvas(master = tab_main)
     frame_text = tk.Frame(master = tab_main, width=500, height=300, padx=50)
     frame_image.pack(side = 'left')
     frame_text.pack()
@@ -68,11 +111,15 @@ if __name__ == "__main__":
     frame_pos = tk.Frame(master = frame_text, padx = 25)
     frame_pos.grid(row=2, sticky='w', pady = 30) #First two rows are taken up by buttons, this frame starts at row 2 of the text frame
     
-    # List of button names
+    # List of button names, positions, and other arrays used in the tk buttons and labels
+    btn_pos_nav = [
+        (5000,5000),
+        (10500,10500)
+    ]
     btn_names = [
-        "Move left \'some amount\'",
-        "Move right \'some amount\'",
-        "Zoom out \'some amount\'",
+        f"Move to {btn_pos_nav[0]}",
+        f"Move to {btn_pos_nav[1]}",
+        "Zoom in \'some amount\'",
         "Zoom out \'some amount\'",
     ]
     btn_positions = [[0, 0], [0, 1], [1, 0], [1, 1]]
@@ -84,67 +131,52 @@ if __name__ == "__main__":
     # ]
     pos_names = [
         "Pos X",
-        "Pos Y"
-    ]
-    positions = [
-        '100nm',
-        '200nm'
+        "Pos Y",
+        "Focus Z"
     ]
 
-    image = tk.Label(master = frame_image, image = img) #Creates a Label wiget
-    image.pack()
-    image_result = tk.Label(master = frame_image, image = stitched_img)
+    # Image handling
+    image_path = "Images\Screenshot 2024-07-16 154534.png"
+    # Gets the image in a ImageTK type
+    img = ImageTk.PhotoImage(Image.open(image_path).resize((400,400))) #Need to check if resize affects calibration/measurement.
+    stitched_img = ImageTk.PhotoImage(Image.open(image_path).resize((400,400)))
+
+    # image = tk.Label(master = frame_image, image = img) #Creates a Label wiget
+    # image.resize((int(image.width * 1.2), int(image.height * 1.2)))
+    # image.pack() #packs the Label widget into the frame
+    image_result = tk.Label(frame_image, image = stitched_img)
     image_result.pack()
 
     # Create buttons and place them in the grid
     # Buttons required - origin, set start, set end
     for i in range(len(btn_names)):
-        button = tk.Button(frame_text, text = btn_names[i], width = 22, height = 2, relief = 'groove') #, command = command_list
-        button.grid(row = btn_positions[i][0], column = btn_positions[i][1], padx=30, pady=30) #padding around the buttons, not the text in the buttons.
+        button = tk.Button(frame_text, text = btn_names[i], width = 22, height = 2, relief = 'groove', command = move_and_wait(pos=btn_pos_nav[i])) #, command = command_list[1]
+        button.grid(row = btn_positions[i][0], column = btn_positions[i][1], padx=30, pady=30) # padding around the buttons, not the text in the buttons.
     
     # Positions
+    # Positions required - Live view of X,Y, and Z (focus)
     for i in range(len(pos_names)):
         label = tk.Label(frame_pos, text = pos_names[i], padx = 10, pady = 5)
         label.grid(row = i, column = 0)
-    for i in range(len(positions)):
-        label = tk.Label(frame_pos, text = positions[i], padx = 5, bg='lightgrey', width = 10)
+    for i in range(len(pos_names)):
+        label = tk.Label(frame_pos, text = f'{get_pos(stages=i+3)} nm', padx = 5, bg='lightgrey', width = 10)
+        label.grid(row = i, column = 1)
+    for i in range(2):
+        label = tk.Label(frame_pos, text = 'pixels', padx = 5, bg='lightgrey', width = 10)
         label.grid(row = i, column = 1)
     
-    # Create a calibration tab
+
+    # Calibration tab
+    tab_calibrate = ttk.Frame(notebook)
+    notebook.add(tab_calibrate, text="Calibration")
+
+    # Results tab
+    tab_results = ttk.Frame(notebook)
+    notebook.add(tab_results, text="Results & Analysis")
+
+    # Device tab
+    tab_devices = ttk.Frame(notebook)
+    notebook.add(tab_devices, text="Devices")
     
-    #Runs the window application mainloop
+    # Runs the window application mainloop
     root.mainloop()
-    
-    # # create generic Tk App with just a LiveViewCanvas widget
-    # print("Generating app...")
-    # root = tk.Tk()
-    # root.title('Trying to view an image in a window')
-    # camera_widget = LiveViewCanvas(parent=root, image_queue=image_acquisition_thread.get_output_queue())
-    # vel = 50      
-
-    # # create generic Tk App with just a LiveViewCanvas widget
-    # print("Generating app...")
-    # root = tk.Tk()
-    # root.title(camera.name)
-    # image_acquisition_thread = ImageAcquisitionThread(camera)
-    # camera_widget = LiveViewCanvas(parent=root, image_queue=image_acquisition_thread.get_output_queue())
-    # vel = 50            
-
-    # print("Setting camera parameters...")
-    # camera.frames_per_trigger_zero_for_unlimited = 0
-    # camera.arm(2)
-    # camera.issue_software_trigger()
-
-    # print("Starting image acquisition thread...")
-    # image_acquisition_thread.start()
-
-    # print("App starting")
-    # root.mainloop()
-
-    # print("Waiting for image acquisition thread to finish...")
-    # image_acquisition_thread.stop()
-    # image_acquisition_thread.join()
-
-    # print("Closing resources...")
-
-    # print("App terminated. Goodbye!")
--- test_GUI.py
+++ test_GUI.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import random
+from PIL import Image, ImageTk
 
 # Initialize the main window
 root = tk.Tk()
@@ -18,6 +19,55 @@ notebook.add(tab1, text="Tab 1")
 label1 = tk.Label(tab1, text="This is Tab 1", font=("Arial", 16))
 label1.pack(pady=20)
 
+# Trying to zoom in on cropped image
+canvas = tk.Canvas(tab1, width = 1600, height = 650)
+canvas.pack()
+# Image handling
+image_path = "Images\Screenshot 2024-07-16 154534.png"
+original_image = Image.open(image_path) #Gets the image from file
+tk_image = ImageTk.PhotoImage(original_image) #Creates image format or something with ImageTk
+canvas.create_image(650, 0, anchor=tk.NW, image=tk_image) #Displays the image
+zoom_factor = 3
+region = [0, 0, 200, 200]
+cropped_image = original_image.crop(region) #crops the image to the pixels desired
+resized_image = cropped_image.resize(
+            (int((region[2] - region[0]) * zoom_factor),
+             int((region[3] - region[1]) * zoom_factor)),
+            Image.LANCZOS) #resizes it to zoom into the cropped area
+tk_image_zoom = ImageTk.PhotoImage(resized_image)
+canvas.create_image(0,0, anchor = tk.NW, image = tk_image_zoom)
+
+# Create a button that moves the image in x and y by 50 pixels
+def move(dx=0, dy=0):
+    region[0] += 50*dx
+    region[2] += 50*dx
+    region[1] += 50*dy
+    region[3] += 50*dy
+    print(region)
+    cropped_image = original_image.crop(region) #crops the image to the pixels desired
+    resized_image = cropped_image.resize(
+                (int((region[2] - region[0]) * zoom_factor),
+                int((region[3] - region[1]) * zoom_factor)),
+                Image.LANCZOS) #resizes it to zoom into the cropped area - not dependent on location, just width and height
+    tk_image_zoom = ImageTk.PhotoImage(resized_image)
+    canvas.delete("all")
+    canvas.create_image(0,0, anchor = tk.NW, image = tk_image_zoom)
+
+button_frame = tk.Frame(tab1)
+button_frame.pack(pady=10)
+
+btn_up = tk.Button(button_frame, text="Up", command=move(dy=1))
+btn_up.grid(row=0, column=1)
+
+btn_left = tk.Button(button_frame, text="Left", command=move(dx=-1))
+btn_left.grid(row=1, column=0)
+
+btn_right = tk.Button(button_frame, text="Right", command=move(dx=1))
+btn_right.grid(row=1, column=2)
+
+btn_down = tk.Button(button_frame, text="Down", command=move(dy=-1))
+btn_down.grid(row=2, column=1)
+
 # Create the second tab
 tab2 = ttk.Frame(notebook)
 notebook.add(tab2, text="Tab 2")
@@ -32,10 +82,10 @@ def randNum():
 root.columnconfigure(0, minsize=150)
 root.rowconfigure([0, 1], minsize=50)
 
-btn_roll = tk.Button(text = "Roll!",command=randNum)
-btn_roll.grid(row=0, column=0, sticky="nsew")
-lbl_roll = tk.Label()
-lbl_roll.grid(row=1, column=0)
+btn_roll = tk.Button(master = tab2, text = "Roll!", command=randNum)
+btn_roll.pack()
+lbl_roll = tk.Label(master = tab2)
+lbl_roll.pack()
 
 # Start the Tkinter event loop
 root.mainloop()
